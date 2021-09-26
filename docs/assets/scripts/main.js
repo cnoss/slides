417ad2501a13efbac90a8aafa82de866b0bd1305
cnoss <christian.noss@th-koeln.de>
2021-09-26 16:32:27 +0200

    Prepared Deployment

---
+++ docs/assets/scripts/main.js
@@ -0,0 +1,55 @@
+const fitTextElements = document.querySelectorAll(".js-fit-text");
+
+
+
+Reveal.on( 'ready', event => {
+  const figures = document.querySelectorAll(".slide figure");
+  figures.forEach(figure => {
+    figure.addEventListener("click", (ev) => {
+      figure.classList.toggle("zoom");
+    });
+  });
+} );
+
+Reveal.on('slidechanged', event => {
+  fitTextElements.forEach(ele=>{
+    window.fitText( ele );
+  });
+
+  const BUs = event.currentSlide.querySelectorAll(".bu");
+  BUs.forEach(bu => {
+    bu.classList.add("is-active");
+  });
+
+  const delayedItems = event.currentSlide.querySelectorAll(".js-delay");
+  delayedItems.forEach(item => {
+    item.classList.add("has-delay");
+  });
+
+  const info = event.currentSlide.querySelector(".info");
+  if (info) {
+    info.addEventListener("click", (ev) => { 
+      info.classList.toggle("is-active");
+      event.currentSlide.querySelector("blockquote").classList.toggle("is-passive");
+    });
+  }
+
+  if (!event.previousSlide) return;
+  
+  const lastBUs = event.previousSlide.querySelectorAll(".bu");
+  lastBUs.forEach(bu => {
+    bu.classList.remove("is-active");
+  });
+
+  const lastDelayedItems = event.previousSlide.querySelectorAll(".js-delay");
+  lastDelayedItems.forEach(item => {
+    item.classList.remove("has-delay");
+  });
+
+  /* const lastFigures = event.previousSlide.querySelectorAll("figure");
+  lastFigures.forEach(figure => {
+    figure.removeEventListener("click", (ev) => {
+      toggleZoom(figure);
+    }, true);
+  });*/
+} );
