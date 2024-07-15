7bd818667ae4dd4e71cda2abf679db514213a6d5
cnoss <cnoss@users.noreply.github.com>
2024-07-15 12:35:07 +0000

    deploy: 4b43d3a8ef8b70dc37ff1f5ef183640a82a0bff9

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,47 @@
 const fitTextElements = document.querySelectorAll(".js-fit-text");
 
+/* Functions
+############################################################################ */
 
+const addCopyToClipboard = () => {
+  const present = document.querySelector(".mi-slide.present");
+  const codeBlocks = present.querySelectorAll("pre .hljs, pre .code");
+
+  codeBlocks.forEach(codeBlock => {
+    const button = document.createElement("button");
+    button.classList.add("copy-button");
+    button.classList.add("icon");
+    button.classList.add("icon-copy");
+    button.textContent = "assignment";
+
+    button.addEventListener("click", (ev) => {
+      const text = codeBlock.textContent.replace(/assignment/g, "");
+      navigator.clipboard.writeText(text);
+    });
+    codeBlock.appendChild(button);
+  } );  
+};
+
+const reorderFooter = () => {
+  const present = document.querySelector(".mi-slide.present");
+  if(!present) return;
+
+  const presentChild = present.querySelector(".present");
+  if(!presentChild) return;
+
+  const footer = present.querySelector("footer");
+  if(!footer) return;
+  
+  presentChild.appendChild(footer);
+
+  const bu = presentChild.querySelector(".bu");
+  if(bu) return;
+
+  footer.classList.add("is-active");
+}
+
+/* Main
+############################################################################ */
 
 Reveal.on( 'ready', event => {
 
@@ -21,6 +62,10 @@ Reveal.on( 'ready', event => {
 } );
 
 Reveal.on('slidechanged', event => {
+
+  addCopyToClipboard();
+  reorderFooter();
+
   fitTextElements.forEach(ele=>{
     window.fitText( ele );
   });
@@ -46,6 +91,8 @@ Reveal.on('slidechanged', event => {
   lastDelayedItems.forEach(item => {
     item.classList.remove("has-delay");
   });
+
+  
 
   /* const lastFigures = event.previousSlide.querySelectorAll("figure");
   lastFigures.forEach(figure => {
